a857cce3c23b767ed4b94ecf9a845c0e98b22c53
calmwalter <calmwalter@outlook.com>
2021-01-07 10:43:09 +0800

    test

---
+++ src/main/java/qmstore/activity_category/dao/ActivityCategoryMapper.java
@@ -0,0 +1,13 @@
+package qmstore.activity_category.dao;
+
+import qmstore.activity_category.pojo.ActivityCategory;
+
+import java.util.ArrayList;
+
+public interface ActivityCategoryMapper {
+    ArrayList<ActivityCategory> findAll();
+    void add(ActivityCategory activityCategory);
+    int update(ActivityCategory activityCategory);
+    int delete(int id);
+    int find(int id);
+}
