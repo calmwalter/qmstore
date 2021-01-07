1522cb1fd5e9399d858df1c87369088c107ccc57
calmwalter <calmwalter@outlook.com>
2021-01-07 10:43:09 +0800

    test

--- src/main/java/qmstore/commodity/controller/CommodityController.java
+++ src/main/java/qmstore/commodity/controller/CommodityController.java
@@ -36,6 +36,7 @@ public class CommodityController {
     @PostMapping("/add")
     public Commodity add(@RequestBody Commodity commodity, @DataAuth User user) throws IOException {
         if(user != null){
+
         }
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
