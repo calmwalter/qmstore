f7a1dd7a0f9fedb709096acdadf46c1dfc4515d4
typeme <typeme3@163.com>
2021-01-09 10:50:05 +0800

    修改数据类型

--- src/main/java/qmstore/shop_cart/dao/ShopCartDao.java
+++ src/main/java/qmstore/shop_cart/dao/ShopCartDao.java
@@ -4,10 +4,12 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import qmstore.shop_cart.pojo.ShopCart;
 
+import java.util.List;
+
 @Repository
 public interface ShopCartDao {
-    public ShopCart getShopCartByUser(String userId);
-    public ShopCart getShopCartByGoods(String goodsId);
+    public List<ShopCart> getShopCartByUser(String userId);
+    public List<ShopCart> getShopCartByGoods(String goodsId);
     public void addShopCart(ShopCart shopCart);
     public void deleteShopCartByUserAndGoodsId(@Param("userId")String userId, @Param("GoodsId") String GoodsId);
     public void updateShopCart(ShopCart shopCart);
